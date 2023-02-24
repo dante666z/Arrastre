fe7e6583d46e1fc4e9faca924ac1e27e4dca85f7
Zuriel Zarate Montesinos <mdantez666@gmail.com>
2023-02-24 12:52:00 -0600

    Arrastre funcionando con Behaviors MVVMAgregar archivos de proyecto.

---
+++ Arrastre/Behaviors/Arrastre.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System.Linq;
+
+namespace Arrastre.Behaviors
+{
+    public class Arrastre : Behavior<View>
+    {
+        public double Verticaltransation { get; set; }
+        PanGestureRecognizer panGestureRecognizer = new PanGestureRecognizer();
+        protected override void OnAttachedTo(View bindable)
+        {
+            panGestureRecognizer.PanUpdated += PanGestureRecognizer_PanUpdated;
+            bindable.GestureRecognizers.Add(panGestureRecognizer);
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(View bindable)
+        {
+            panGestureRecognizer.PanUpdated -= PanGestureRecognizer_PanUpdated;
+            bindable.GestureRecognizers.Remove(panGestureRecognizer);
+            base.OnDetachingFrom(bindable);
+        }
+        private async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    if (e.TotalY > 0)
+                    {
+                        await ((View)sender).TranslateTo(0, e.TotalY);
+                        Verticaltransation = e.TotalY;
+                    }
+                    break;
+                case GestureStatus.Completed:
+                    if (Verticaltransation > 100)
+                    {
+                        await ((View)sender).TranslateTo(0, 200);
+                        if (PopupNavigation.Instance.PopupStack.Any())
+                        {
+                            await PopupNavigation.Instance.PopAsync();
+                        }
+                    }
+                    else
+                    {
+                        await ((View)sender).TranslateTo(0, e.TotalY);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Arrastre/Paginahijo.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup.Services;
+
+namespace Arrastre
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Paginahijo : PopupPage
+    {
+        public Paginahijo()
+        {
+            InitializeComponent();
+        }
+        public double Verticaltransation { get; set; }
+        private async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch(e.StatusType)
+            {
+                case GestureStatus.Running:
+                    if(e.TotalY > 0)
+                    {
+                        await gridprincipal.TranslateTo(0, e.TotalY);
+                        Verticaltransation = e.TotalY;
+                    }
+                    break;
+                case GestureStatus.Completed:
+                    if (Verticaltransation > 100)
+                    {
+                        await gridprincipal.TranslateTo(0, 200);
+                        if(PopupNavigation.Instance.PopupStack.Any())
+                        {
+                            await PopupNavigation.Instance.PopAsync();
+                        }
+                    }
+                    else
+                    {
+                        await gridprincipal.TranslateTo(0, e.TotalY);
+                    }
+                    break;
+            }
+            lbly.Text = e.TotalY.ToString();
+
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
